d52d3c5a73f4c60ffc5ff92903ed4490bd2c4e2c
Kristjan <ucloud16085@gmail.com>
2021-10-18 21:00:18 +0300

    1.6 Transpodi organiseerimine

---
+++ Praks1/1.6 Transpordi organiseerimine.py
@@ -0,0 +1,16 @@
+ühe_bussi_istekohad = int(input("Sisestage bussi kohtade arv: "))
+inimesi = int(input("Sisestage inimeste arv: "))
+busside_arv = int(input("Sisestage busside arv: "))
+istekohad = ühe_bussi_istekohad * busside_arv
+mahajäänud_inimesed = inimesi % istekohad
+print(input("Mahajäänud inimesi on: " + str(mahajäänud_inimesed)))
+
+##mahajäänud = int(input("Sisestage mahajäänud inimeste arv: "))
+##inimeste arvu
+##kohtade aru ühes bussis
+##busside arvu
+##bussidesse mahtunud inimeste arvu
+##mahajäänud inimeste arvu
+#5 bussi
+#234 inimest
+## 36 istet

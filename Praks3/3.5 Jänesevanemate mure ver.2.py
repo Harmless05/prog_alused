ccc838d41da2494893b6f86c1f7624d55a2ffbaf
Kristjan <ucloud16085@gmail.com>
2021-11-16 10:33:21 +0200

    Praks3

--- Praks3/3.5 Jänesevanemate mure ver.2.py
+++ Praks3/3.5 Jänesevanemate mure ver.2.py
@@ -1,7 +1,9 @@
 arv = int(input("Sisestage ringide arv: "))
 porgandid = 0
+porgandid_karbis = 0
 ring = 1
 while ring <= arv:
-    porgandid = porgandid + ring
+    porgandid_karbis = porgandid_karbis + ring
+    porgandid = porgandid + porgandid_karbis
     ring = ring + 1
-print("Porgandite koguarv or " + str(porgandid))
+print("Porgandite koguarv on " + str(porgandid))

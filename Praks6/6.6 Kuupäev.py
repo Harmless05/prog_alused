ae8a23fc36d304d4b52a0fc164f325e6facccffc
Kristjan <ucloud16085@gmail.com>
2021-12-01 13:17:00 +0200

    Praks6

--- Praks6/6.6 Kuupäev.py
+++ Praks6/6.6 Kuupäev.py
@@ -1,6 +1,12 @@
 def kuu_nimi(kuu):
-    
-kuupäev = int(input("Sisesta kuupäev kujul DD.MM.YYYY: "))
+    kuud = ["jaanuar", "veebruar", "märts", "april", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"]
+    return kuud[int(kuu) -1]
 
 def kuupäev_sõnena(kuupäev):
-    
+    kuupäevad = kuupäev.split(".")
+    kuupäev = kuupäevad[0] + ". " + kuu_nimi(kuupäevad[1]) + " " + kuupäevad[2] + ". a"
+    return kuupäev
+
+kuupäev = str(input("Sisestage kuupäev kujul DD.MM.YYYY: "))
+print(str(kuupäev_sõnena(kuupäev)))
+
